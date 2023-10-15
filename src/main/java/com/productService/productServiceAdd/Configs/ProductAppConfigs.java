4e24e66615613fecabadd7e95b8e6c0ba010e06f
Anericokakai <challykiddoh@gmail.com>
2023-10-15 21:28:24 +0300

    microservice done

--- src/main/java/com/productService/productServiceAdd/Configs/ProductAppConfigs.java
+++ src/main/java/com/productService/productServiceAdd/Configs/ProductAppConfigs.java
@@ -2,23 +2,38 @@ package com.productService.productServiceAdd.Configs;
 
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ProductAppConfigs {
+
+    @Value("${shopsservice.base.url}")
+    private String adressBaseURL;
 
     @Bean
     public ModelMapper modelMapper(){
         return  new ModelMapper();
     }
 
+
+@LoadBalanced
     @Bean
     public RestTemplate restTemplate(){
-        return  new RestTemplate();
-    }
+        return new RestTemplate();
 
+    }
+@Bean
+public WebClient webClient(){
+        return WebClient
+                .builder()
+                .baseUrl(adressBaseURL)
+                .build();
+}
 
 }
 
--- src/main/java/com/productService/productServiceAdd/Services/ProductServiceImpl.java
+++ src/main/java/com/productService/productServiceAdd/Services/ProductServiceImpl.java
@@ -7,8 +7,14 @@ import com.productService.productServiceAdd.tdo.ProductResponse;
 import com.productService.productServiceAdd.tdo.ShopResponse;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -21,8 +27,17 @@ public class ProductServiceImpl  implements ProductService{
 private final ProductRepository productRepository;
 
 private final RestTemplate restTemplate;
-private final ModelMapper modelMapper;
 
+private final ModelMapper modelMapper;
+//@Value("${shopsservice.base.url}")
+//private String adressBaseURL;
+
+
+    private  final WebClient webClient;
+
+    private  final LoadBalancerClient loadBalancerClient;
+
+//private  final DiscoveryClient discoveryClient;
 
     @Override
     public String createNewProduct(ProductRequest productRequest) {
@@ -46,7 +61,34 @@ private final ModelMapper modelMapper;
         Products products= productRepository.findById(id).get();
         ProductResponse productResponse=modelMapper.map(products,ProductResponse.class);
 
-      ShopResponse  shopResponse=restTemplate.getForObject("http://localhost:8081/v2/api/shops/{shopId}", ShopResponse.class,shopId);
+//        ! USING THE WEBCLIENT
+//
+//        ShopResponse shopResponse=webClient
+//                .get()
+//                .uri("/shops/"+shopId)
+//                .retrieve()
+//                .bodyToMono(ShopResponse.class)
+//                .block();
+//        productResponse.setShopResponse(shopResponse);
+
+//! USING REST TEMPLATE
+
+//        List<ServiceInstance> instances= discoveryClient.getInstances("shop-service");
+//
+//        ServiceInstance serviceInstance= instances.get(0);
+//
+
+
+    ServiceInstance serviceInstance=    loadBalancerClient.choose("shop-service");
+          String uri= serviceInstance.getUri().toString();
+          String contextPath=serviceInstance.getMetadata().get("configPath");
+        System.out.println("URLSerivce : "+uri);
+        System.out.println("URLContextPath : "+contextPath);
+
+//   ShopResponse  shopResponse=restTemplate.getForObject(uri+contextPath+"/{shopId}", ShopResponse.class,shopId);
+//        productResponse.setShopResponse(shopResponse);
+
+        ShopResponse  shopResponse=restTemplate.getForObject("http://SHOP-SERVICE"+contextPath+"/{shopId}", ShopResponse.class,shopId);
         productResponse.setShopResponse(shopResponse);
         return  productResponse;
     }
